7dbbe455daea7dfd2c99dacdce4f096891f16051
Andrés Quiceno <asquicenoh97@elpuig.xeill.net>
2019-04-30 19:21:39 +0200

    Added images

--- app/src/main/java/com/example/happyhour/MainActivity.java
+++ app/src/main/java/com/example/happyhour/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.happyhour;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.happyhour.Estructura.Game_mode;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_start;
 
 
     @Override
@@ -17,9 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_start = findViewById(R.id.btn_start);
-
-        btn_start.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Game_mode.class);
