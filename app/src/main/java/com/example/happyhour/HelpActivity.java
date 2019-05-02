79c4b31a7b15b5d57250688f7757bf18e57b52e5
serpipa <sortega@elpuig.xeill.net>
2019-05-02 20:51:56 +0200

    changed icon and bakcground on help activity

--- app/src/main/java/com/example/happyhour/HelpActivity.java
+++ app/src/main/java/com/example/happyhour/HelpActivity.java
@@ -3,12 +3,14 @@ package com.example.happyhour;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.example.happyhour.Estructura.Games;
+import com.example.happyhour.Estructura.Game_mode;
 
 public class HelpActivity extends AppCompatActivity {
 
-    static int wallpaper;
+    private static int wallpaper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,15 @@ public class HelpActivity extends AppCompatActivity {
 
         wallpaper = getIntent().getIntExtra("wallpaper", 1);
 
+        ImageView imageView = findViewById(R.id.fondoPantalla);
+        //Get the resource ID
+        imageView.setImageResource(wallpaper);
+
     }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Intent intent = new Intent(this, Games.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent intent = new Intent(this, Game_mode.class);
         intent.putExtra("wallpaper", wallpaper);
         startActivity(intent);
     }
